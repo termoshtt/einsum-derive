1bed42f238ec5d2ee91e09693187c8bb1e514a6a
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-10-08 17:34:14 +0900

    Allow spaces between indices

--- opt-einsum/src/parser.rs
+++ opt-einsum/src/parser.rs
@@ -1,27 +1,34 @@
 //! Parse einsum operator, e.g. `ij,jk->ik`
 
-use nom::{bytes::complete::*, character::complete::*, multi::*, sequence::*, IResult};
+use nom::{
+    branch::*, bytes::complete::*, character::complete::*, multi::*, sequence::*, IResult, Parser,
+};
+
+pub fn index(input: &str) -> IResult<&str, char> {
+    satisfy(|c| matches!(c, 'a'..='z'))(input)
+}
 
 /// Indices appearing in einsum operator, e.g. `ij`
-pub type Indices<'input> = &'input str;
+pub type Indices = Vec<char>;
 
 pub fn indices(input: &str) -> IResult<&str, Indices> {
-    alpha1(input)
+    many0(alt((index.map(|c| Some(c)), multispace1.map(|_| None))))
+        .map(|chars| chars.into_iter().flatten().collect())
+        .parse(input)
 }
 
 /// Einsum operator, e.g. `ij,jk->ik`
 #[derive(Debug, PartialEq, Eq)]
-pub struct Operator<'input> {
+pub struct Operator {
     /// Input indices, `ij` and `jk`
-    pub inputs: Vec<Indices<'input>>,
+    pub inputs: Vec<Indices>,
     /// Output indices, `ik`
-    pub output: Indices<'input>,
+    pub output: Indices,
 }
 
 pub fn operator(input: &str) -> IResult<&str, Operator> {
     let (input, _head) = multispace0(input)?;
-    let (input, inputs) =
-        separated_list1(tuple((multispace0, char(','), multispace0)), indices)(input)?;
+    let (input, inputs) = separated_list1(char(','), indices)(input)?;
     let (input, _arrow) = tuple((multispace0, tag("->"), multispace0))(input)?;
     let (input, output) = indices(input)?;
     Ok((input, Operator { inputs, output }))
@@ -34,14 +41,24 @@ mod tests {
     use nom::Finish;
 
     #[test]
-    fn parse() {
+    fn test_indices() {
+        let ans = ("", vec!['i', 'j', 'k']);
+        assert_eq!(indices("ijk").finish().unwrap(), ans);
+        assert_eq!(indices("i jk").finish().unwrap(), ans);
+        assert_eq!(indices("ij k").finish().unwrap(), ans);
+        assert_eq!(indices("i j k").finish().unwrap(), ans);
+    }
+
+    #[test]
+    fn test_operator() {
         fn test(input: &str) {
+            dbg!(input);
             let (_, op) = operator(input).finish().unwrap();
             assert_eq!(
                 op,
                 Operator {
-                    inputs: vec!["ij", "jk"],
-                    output: "ik",
+                    inputs: vec![vec!['i', 'j'], vec!['j', 'k']],
+                    output: vec!['i', 'k'],
                 }
             );
         }
@@ -49,10 +66,12 @@ mod tests {
 
         // with space
         test(" ij,jk->ik");
+        test("i j,jk->ik");
         test("ij ,jk->ik");
         test("ij, jk->ik");
+        test("ij,j k->ik");
         test("ij,jk ->ik");
         test("ij,jk-> ik");
-        test("ij,jk->ik ");
+        test("ij,jk->i k");
     }
 }
