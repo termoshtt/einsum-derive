c784292e913fab089cf49bb2289df1969661beac
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-10-13 20:19:44 +0900

    Desired doctest of einsum! proc-macro

--- einsum-derive/src/lib.rs
+++ einsum-derive/src/lib.rs
@@ -1,1 +1,17 @@
-//! proc-macro for einsum
+//! proc-macro based einsum implementation
+
+use proc_macro::TokenStream;
+use proc_macro_error::proc_macro_error;
+
+/// proc-macro based einsum
+///
+/// ```
+/// use einsum_derive::einsum;
+///
+/// let c = einsum!("ij,jk->ik", a, b);
+/// ```
+#[proc_macro_error]
+#[proc_macro]
+pub fn einsum(_input: TokenStream) -> TokenStream {
+    todo!()
+}
