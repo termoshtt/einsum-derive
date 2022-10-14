c652046f607139974a0892863bf2caeb3c56b9de
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-10-14 21:03:52 +0900

    Parse subscripts in proc-macro

--- einsum-derive/src/lib.rs
+++ einsum-derive/src/lib.rs
@@ -1,7 +1,9 @@
 //! proc-macro based einsum implementation
 
+use einsum_solver::subscripts::Subscripts;
 use proc_macro::{TokenStream, TokenTree};
 use proc_macro_error::*;
+use std::str::FromStr;
 
 /// proc-macro based einsum
 ///
@@ -10,17 +12,37 @@ use proc_macro_error::*;
 ///
 /// let c = einsum!("ij,jk->ik", a, b);
 /// ```
+///
+/// ```compile_fail
+/// use einsum_derive::einsum;
+///
+/// // Number of input mismatches!
+/// let c = einsum!("ij,jk->ik", a);
+/// ```
 #[proc_macro_error]
 #[proc_macro]
 pub fn einsum(input: TokenStream) -> TokenStream {
     let mut iter = input.into_iter();
     let subscripts = if let Some(TokenTree::Literal(lit)) = iter.next() {
-        lit.to_string()
+        lit.to_string().trim_matches('"').to_string()
     } else {
         abort_call_site!("einsum! must start with subscript string literal");
     };
+    let mut args = Vec::new();
+    while let Some(arg) = iter.next() {
+        match arg {
+            TokenTree::Ident(ident) => args.push(ident),
+            _ => continue,
+        }
+    }
 
-    dbg!(subscripts);
+    let subscripts = Subscripts::from_str(&subscripts)
+        .ok()
+        .expect_or_abort("Invalid subscripts");
+    if subscripts.inputs.len() != args.len() {
+        abort_call_site!("Argument number mismatch");
+    }
+    dbg!(&subscripts);
 
     todo!()
 }
