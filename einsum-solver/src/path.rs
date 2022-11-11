7c461dd71d7794fca7f8295d02b14de760ad5ea0
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-11-11 17:00:33 +0900

    impl Iterator for Path

--- einsum-solver/src/path.rs
+++ einsum-solver/src/path.rs
@@ -1,33 +1,61 @@
 //! Construct and execute contraction path
 
 use crate::subscripts::*;
+use anyhow::{bail, Result};
+use std::collections::BTreeSet;
 
 /// Contraction path
 #[derive(Debug, PartialEq, Eq)]
 pub struct Path {
-    pub subscripts: Subscripts,
-    pub path: Vec<char>,
+    namespace: Namespace,
+    subscripts: Subscripts,
+    contraction_indices_rev: Vec<char>,
+}
+
+impl Iterator for Path {
+    type Item = Subscripts;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.contraction_indices_rev.is_empty() {
+            return None;
+        }
+        let c = self.contraction_indices_rev.pop().unwrap(/* never empty here */);
+        match self.subscripts.factorize(&mut self.namespace, c) {
+            Ok(None) => None,
+            Ok(Some((first, second))) => {
+                self.subscripts = second;
+                Some(first)
+            }
+            Err(_) => unreachable!(),
+        }
+    }
 }
 
 impl Path {
     /// Manually set contraction order
-    pub fn manual(subscripts: Subscripts, path: Vec<char>) -> Self {
-        Path { subscripts, path }
+    pub fn manual(subscripts: Subscripts, contraction_indices: &[char]) -> Result<Self> {
+        let mut indices_as_set: BTreeSet<char> = BTreeSet::new();
+        for &c in contraction_indices {
+            if !indices_as_set.insert(c) {
+                bail!("Duplicated contraction indices: {}", c);
+            }
+        }
+        if indices_as_set != subscripts.contraction_indices() {
+            bail!("Manually specified indices are not consistent to subscripts");
+        }
+        Ok(Path {
+            namespace: Namespace::init(),
+            subscripts,
+            contraction_indices_rev: contraction_indices.iter().rev().cloned().collect(),
+        })
     }
 
     /// Alphabetical order
-    ///
-    /// ```
-    /// use std::str::FromStr;
-    /// use einsum_solver::{path::Path, subscripts::{Subscripts, Namespace}};
-    ///
-    /// let mut names = Namespace::init();
-    /// let subscripts = Subscripts::from_raw_indices(&mut names, "ij,ji->").unwrap();
-    /// let path = Path::alphabetical(subscripts);
-    /// assert_eq!(path.path, &['i', 'j']);
-    /// ```
     pub fn alphabetical(subscripts: Subscripts) -> Self {
-        let path = subscripts.contraction_indices().into_iter().collect();
-        Path { subscripts, path }
+        let contraction_indices_rev = subscripts.contraction_indices().into_iter().rev().collect();
+        Path {
+            namespace: Namespace::init(),
+            subscripts,
+            contraction_indices_rev,
+        }
     }
 }
