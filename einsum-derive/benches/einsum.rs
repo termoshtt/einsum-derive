7960256c5f344c4a913e8df1b6bf38fd66842f71
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-11-30 17:13:32 +0900

    Add bench for ij,jk,kl case

--- einsum-derive/benches/einsum.rs
+++ einsum-derive/benches/einsum.rs
@@ -3,8 +3,10 @@ use einsum_derive::einsum;
 use ndarray::*;
 use ndarray_linalg::*;
 
-fn ij_jk(c: &mut Criterion) {
+fn einsum_bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("einsum");
+    group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
+
     for &n in &[4, 8, 16, 32, 64, 128] {
         group.bench_with_input(BenchmarkId::new("ij_jk", n), &n, |bench, n| {
             let a: Array2<f64> = random((*n, *n));
@@ -13,8 +15,17 @@ fn ij_jk(c: &mut Criterion) {
                 let _c = einsum!("ij,jk", a.clone(), b.clone());
             })
         });
+
+        group.bench_with_input(BenchmarkId::new("ij_jk_kl", n), &n, |bench, n| {
+            let a: Array2<f64> = random((*n, *n));
+            let b: Array2<f64> = random((*n, *n));
+            let c: Array2<f64> = random((*n, *n));
+            bench.iter(|| {
+                let _c = einsum!("ij,jk,kl", a.clone(), b.clone(), c.clone());
+            })
+        });
     }
 }
 
-criterion_group!(einsum, ij_jk);
+criterion_group!(einsum, einsum_bench);
 criterion_main!(einsum);
